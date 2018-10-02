9d3488833b9539b529e93990daf0fa363e37f0c4
Šimon Baláž <simon.balaz96@gmail.com>
2018-10-02 21:57:19 +0200

    Update test.py

--- 02 - classes/test.py
+++ 02 - classes/test.py
@@ -1,12 +1,11 @@
 import sys 
 import scorelib
-import operator
 
 def main(argv):
     scores = open(sys.argv[1], 'r', encoding='utf_8')
     prints = scorelib.load(scores)
     
-    for item in sorted(prints, key=operator.attrgetter('print_id')):
+    for item in prints:
         item.format()         
 
 
